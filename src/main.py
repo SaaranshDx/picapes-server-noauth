0620f536b5b6e2de5290d7ce58f9a5b4c15cda25
SaaranshDx <saaransh762@gmail.com>
2026-05-20 19:31:08 +0530

    Refactor profile route to use jsonify for JSON responses

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/profile/capeid/<capeid>')
+@app.route('/profile/<capeid>')
 def construct_url(capeid):
-    return json.dumps({'textureURL': f'https://cdn.capeserver.picapes.syanic.org/capes/{capeid}.png'})
+    return jsonify({'textureURL': f'https://cdn.capeserver.picapes.syanic.org/capes/{capeid}.png'})
 
 if __name__ == '__main__':
     app.run(debug=True)
